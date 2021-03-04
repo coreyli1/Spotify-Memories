05c8511df2cb2a7245a7dda408651b94419a5032
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2021-03-04 10:04:48 -0600

    Fixed remove changes and added ability to read songs/names/description after pressing submit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import json
 
 app = Flask(__name__)
+app.secret_key = b'\xb5\xf0>npXE+\x96%\xa62\xc1b\x10\xde'
 
 
 # Spotify URLS
@@ -42,14 +43,11 @@ def index():
 def auth():
     url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
     auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
-    print(auth_url)
     return redirect(auth_url)
 
 @app.route("/callback/q")
 def callback():
     # Auth Step 4: Requests refresh and access tokens
-
-    print(request.args)
 
     auth_token = request.args['code']
     print("auth_token", auth_token)
@@ -75,16 +73,33 @@ def callback():
     authorization_header = {"Authorization": "Bearer {}".format(access_token)}
 
     # Get recently played
-    user_profile_api_endpoint = "{}/me/player/recently-played?limit=50".format(SPOTIFY_API_URL)
+    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
     profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
     profile_data = json.loads(profile_response.text)
 
-    # print(profile_data['items'])
+    session['access_token'] = access_token
+    session['profile_data'] = profile_data
+    return redirect('/checklist') 
 
+@app.route("/checklist", methods=["GET", "POST"])
+def checklist():
 
-    #return redirect('/checklist') 
-    return render_template("checklist.html", pd=profile_data['items'])
+    access_token = session['access_token']
+    authorization_header = {"Authorization": "Bearer {}".format(access_token)}
 
+    user_playlist_api_endpoint = "{}/me/player/recently-played?limit=50".format(SPOTIFY_API_URL)
+    playlist_response = requests.get(user_playlist_api_endpoint, headers=authorization_header)
+    playlist_data = json.loads(playlist_response.text)
+
+    if request.method == "POST":
+        result = list(dict(request.form).values())
+        songs = result[2:len(result)-1]
+        name = result[0]
+        description = result[1]
+
+        print(songs, name, description)
+
+    return render_template("checklist.html", pd=playlist_data['items'])
 
 
 if __name__ == "__main__":
